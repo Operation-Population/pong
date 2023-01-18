0ac1589ada5081eb6f233a1f335b7ec00e12f63b
Giorgio <65674128+Gaffomdz@users.noreply.github.com>
2023-01-18 16:49:09 +0300

    some functionality

--- src/game.ts
+++ src/game.ts
@@ -1,21 +1,92 @@
 import { Dash_TriggerZone, Dash_Tweaker } from "dcldash"
 let ats = 0
 let bts = 0
+//Entities
+const disk = new Entity()
+const goalPost = new Entity()
+const scoreBoard = new Entity()
+const arche1 = new Entity()
+const arche2 = new Entity()
+const glass = new Entity()
+const ground = new Entity()
+const surround = new Entity()
+const floor = new Entity()
+
+//---
+const camera = new Camera()
+
+//adding the models
+disk.addComponent(new GLTFShape('models/PongChamp_Assets_disk.glb'))
+goalPost.addComponent(new GLTFShape('models/PongChamp_Assets_GoalPosts.glb'))
+scoreBoard.addComponent(new GLTFShape('models/PongChamp_Assets_ScoreBoard.glb'))
+arche1.addComponent(new GLTFShape('models/PongChamp_Env_arche1.glb'))
+arche2.addComponent(new GLTFShape('models/PongChamp_Env_arches.glb'))
+glass.addComponent(new GLTFShape('models/PongChamp_Env_ArenaGlass.glb'))
+ground.addComponent(new GLTFShape('models/PongChamp_Env_ArenaGround.glb'))
+surround.addComponent(new GLTFShape('models/PongChamp_Env_ArenaSurround.glb'))
+floor.addComponent(new GLTFShape('models/PongChamp_Env_Floor.glb'))
 
 
 
+
+//arche1.setParent(ground)
+//arche2.setParent(ground)
+goalPost.setParent(ground)
+surround.setParent(ground)
+glass.setParent(ground)
+scoreBoard.setParent(ground)
+
+
+
+//Position fill
+
+ground.addComponent(new Transform({
+  position: new Vector3(48.000, 0.880, 59.000),
+  scale: new Vector3(1.000, 1.000, 1.000),
+  rotation: new Quaternion().setEuler(0.000, 270.000, 0.000)
+}))
+arche2.addComponent(new Transform({
+  position: new Vector3(22.000, 0.000, 0.000),
+  scale: new Vector3(1.000, 1.000, 1.000),
+  rotation: new Quaternion().setEuler(0.000, 0.000, 0.000),
+}))
+scoreBoard.addComponent(new  Transform({
+  position: new Vector3(0.000, 25.000, 0.000),
+  scale: new Vector3(1.000, 1.000, 1.000),
+  rotation: new Quaternion().setEuler(0.000, 0.000, 0.000),
+}))
+
+
+
+engine.addEntity(ground)
+
+
+//Server
+const ws = new WebSocket("ws://localhost:13370")
 
 //Create ball entity
 const ball = new Entity()
-ball.addComponent(new Transform({ position: new Vector3(1, 0.8, 1) }))
-ball.addComponent(new SphereShape())
+ball.addComponent(new Transform({ position: new Vector3(47.43,9.48,58.58) }))
+ball.addComponent(new GLTFShape('models/PongChamp_Assets_disk.glb'))
 ball.addComponent(
   new OnPointerDown((e) => {
-    if (ball.hasComponent(BallMovement)) ball.getComponent(BallMovement).direction.z = ball.getComponent(BallMovement).direction.z * -1
-    else ball.addComponent(new BallMovement(new Vector3(1.5, 0, 5.4)))
+    log('test')
+    const userPosition = camera.position
+    const ballPosition = ball.getComponent(Transform).position
+    const direction = userPosition.subtract(ballPosition).multiply(new Vector3(-1,0,-1))
+    ws.send(JSON.stringify(direction.asArray()))
   })
 )
+
+
 engine.addEntity(ball)
+
+ws.onmessage = (ev)=>{
+  const json = JSON.parse(ev.data)
+  if (ball.hasComponent(BallMovement)) ball.getComponent(BallMovement).direction = new Vector3(json[0],json[1],json[2])
+  else ball.addComponent(new BallMovement(new Vector3(6.5, 0, 0.4)))
+}
+
 
 //Create ball movement component
 @Component('BallMovement')
@@ -33,36 +104,38 @@ class BallSystem implements ISystem {
         .getComponent(Transform)
         .position.addInPlace(ball.getComponent(BallMovement).direction.multiply(new Vector3().setAll(dt / 1)))
     }
-    if (ball.getComponent(Transform).position.x > 14) {
+    if (ball.getComponent(Transform).position.x > 65) {
       ball.getComponent(BallMovement).direction.x = ball.getComponent(BallMovement)?.direction.x * -1
     }
-    if (ball.getComponent(Transform).position.x < 1) {
+    if (ball.getComponent(Transform).position.x < 32) {
       ball.getComponent(BallMovement).direction.x = ball.getComponent(BallMovement)?.direction.x * -1
     }
-    if (ball.getComponent(Transform).position.z > 63) {
-     bTeamScore()
+    if (ball.getComponent(Transform).position.z > 96) {
+      bTeamScore()
     }
-    if (ball.getComponent(Transform).position.z < 1) {
-     aTeamScore()
+    if (ball.getComponent(Transform).position.z < 21) {
+      aTeamScore()
     }
   }
 }
 engine.addSystem(new BallSystem())
 
-
-
 function aTeamScore() {
   ats++
-  reset()  
+  log(ats)
+  reset()
 }
 function bTeamScore() {
   bts++
+  log(bts)
   reset()
 }
 function reset() {
-  log("heyehey")
+  
   ball
-  .addComponentOrReplace(new Transform({position: new Vector3(9.20,0.88,20.44)}))
+    .addComponentOrReplace(new Transform({ position: new Vector3(47.43,9.46,58.58) }))
+
+  ball.removeComponent(BallMovement)
 }
 
 
